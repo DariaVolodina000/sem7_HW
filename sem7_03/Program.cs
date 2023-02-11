34dd9e12d1de3c1ff9630fb4c37c397b18084845
Daria <dashavolodina@gmail.com>
2023-02-11 13:04:25 +0800

    Подсчет среднеарифмитического в каждом столбце двухмерного массива

---
+++ sem7_03/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(-9, 10);
+        }
+    }
+}
+
+void CountColumn(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = sum + arr[i, j];
+        }
+        double result = sum / arr.GetLength(1);
+        Console.WriteLine($"Среднее арифмитическое столбца {j + 1} =  {result}");
+    }
+}
+
+int[,] line = new int[5, 5];
+FillArray(line);
+PrintArray(line);
+
+CountColumn(line);
